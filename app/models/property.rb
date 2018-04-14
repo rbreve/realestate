d18dade45d20fd9e3307f84aa0f1cd7cdd1d563b
Roberto Breve <rbreve@Robertos-MacBook-Pro.local>
2018-04-14 01:22:08 -0600

    ui better

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,9 +1,11 @@
 class Property < ApplicationRecord
   belongs_to :neighborhood
-  belongs_to :category
+ 
+  belongs_to :category, counter_cache: true
   belongs_to :city
   belongs_to :state
   has_many :photos  
+
 
   CURRENCY = ["Lps", "$"]
 
